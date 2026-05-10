26fbc63f24f6c726133f0bf884ebb1f00ae469cc
Ubuntu <ubuntu@ip-172-31-41-6.ap-northeast-1.compute.internal>
2026-05-10 20:40:52 +0000

    Deploy Kafka streaming platform to EKS with Prometheus and Grafana monitoring

--- consumer/consumer.py
+++ consumer/consumer.py
@@ -1,19 +1,45 @@
 from kafka import KafkaConsumer
+from prometheus_client import start_http_server, Counter
 import json
+import time
 
-consumer = KafkaConsumer(
-    'user_activity',
-    bootstrap_servers='kafka-service:9092',
-    auto_offset_reset='earliest',
-    value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+messages_received = Counter(
+    'messages_received_total',
+    'Total messages received'
 )
 
-print("Listening for messages...")
+high_value_alerts = Counter(
+    'high_value_alerts_total',
+    'Total high value alerts'
+)
+
+start_http_server(8001)
+
+while True:
+    try:
+        consumer = KafkaConsumer(
+            'user_activity',
+            bootstrap_servers='kafka:9092',
+            auto_offset_reset='earliest',
+            value_deserializer=lambda x: json.loads(x.decode('utf-8'))
+        )
+
+        print("Connected to Kafka!", flush=True)
+        break
+
+    except Exception as e:
+        print(f"Waiting for Kafka... {e}", flush=True)
+        time.sleep(5)
+
+print("Listening for messages...", flush=True)
 
 for message in consumer:
     data = message.value
 
-    print(f"Received: {data}")
+    messages_received.inc()
+
+    print(f"Received: {data}", flush=True)
 
     if data["value"] > 80:
-        print("⚠️ ALERT: High value activity detected!")
+        high_value_alerts.inc()
+        print("⚠️ ALERT: High value activity detected!", flush=True)
